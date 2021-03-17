e8ee188b547a10258c683be37ac6741f21e769a2
David-HUMBLOT <humblot.david1@gamil.com>
2021-03-17 20:49:39 +0400

    Added a Procfile

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '96ec5b8cfeab7b6558846ff66a98f5dbddfcd2fe',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '96ec5b8cfeab7b6558846ff66a98f5dbddfcd2fe',
+    ),
+  ),
+);
